a4659bc6ea6ae0692e642384ca6554ecd2ed6450
sergey.shaldanov <sergey.shaldanov@akvelon.com>
2016-11-26 09:34:13 +0300

    Improved file handler.

--- file-handler.js
+++ file-handler.js
@@ -2,13 +2,71 @@
 
 var cv = require('opencv');
 
+var GREEN_PLATE_COLOR_AVG = [ 55, 175, 120 ];
+var BROWN_PLATE_COLOR_AVG = [ 80, 113, 142 ];
+
+var MINIMAL_ALLOWED_DISTANCE = 20;
+
 var handle = function (url) {
   cv.readImage(url, function (err, im) {
-    var result = im.matchTemplate('./files/p1.jpg', 0);
-    console.log("x:" + result[1] + " y:" + result[2]);
+    var player = im.matchTemplate('./files/p1.jpg', 0);
+    console.log("x:" + player[1] + " y:" + player[2]);
 
-    result = im.matchTemplate('./files/plate.jpg', 5);
-    var matches = result[0].templateMatches(0.10, 1.0, 5, false);
+    var matched = im.matchTemplate('./files/plate.jpg', 5)[0];
+
+    var im_converted = im.clone();
+
+    im_converted.convertGrayscale();
+    im_converted.canny(0, 100);
+    im_converted.dilate(5);
+
+    var contours = im_converted.findContours();
+
+    var data = {
+    	plates: [],
+    	player: { x: player[1], y: player[2] }
+    };
+
+	for (var i = 0; i < contours.size(); i++) {
+
+	    var rect = contours.boundingRect(i);
+
+	    console.log("rect: " + JSON.stringify(rect));
+
+	    // It is not a plate guys.
+	    if (rect.width < 100 || rect.width > 160) {
+	    	console.log("is not a plate");
+	    	continue;
+	    }
+
+	    var plateImage = im.crop(rect.x, rect.y, rect.width, rect.height);
+
+	    plateImage.resize(1, 1);
+
+	    var plateAvgColor = plateImage.pixel();
+
+	    var difference = Math.sqrt(
+	    	Math.pow(BROWN_PLATE_COLOR_AVG[0] - plateAvgColor[0], 2) + 
+	    	Math.pow(BROWN_PLATE_COLOR_AVG[1] - plateAvgColor[1], 2) +
+	    	Math.pow(BROWN_PLATE_COLOR_AVG[2] - plateAvgColor[2], 2));
+
+	    console.log("diff: " + difference);
+
+	    // It is a brown plate guys.
+	    if (difference <= MINIMAL_ALLOWED_DISTANCE) {
+	    	console.log("is brown plate");
+	    	continue;
+	    }
+
+	    im.drawContour(contours, i, [0, 0, 255]);
+
+	    data.plates.push({
+	    	x: rect[0],
+	    	y: rect[1]
+	    });
+	}
+
+	im.save('out.jpg')
 
     console.log("handle: " + url);
   });
