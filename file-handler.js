b2325907b9ebddc076832d1ae1d8017df3266f09
vyacheslav.lonshchakov <Exeil@inbox.ru>
2016-11-26 08:04:37 +0300

    Added detect person.

--- file-handler.js
+++ file-handler.js
@@ -3,7 +3,15 @@
 var cv = require('opencv');
 
 var handle = function (url) {
-  console.log("handle: " + url);
+  cv.readImage(url, function (err, im) {
+    var result = im.matchTemplate('./files/p1.jpg', 0);
+    console.log("x:" + result[1] + " y:" + result[2]);
+
+    result = im.matchTemplate('./files/plate.jpg', 5);
+    var matches = result[0].templateMatches(0.10, 1.0, 5, false);
+
+    console.log("handle: " + url);
+  });
 };
 
 module.exports = {
--- specs/test.js
+++ specs/test.js
@@ -2,11 +2,11 @@ var fileHandler = require('../file-handler.js')
 var neuralNetwork = require('../neural-network.js')
 var dataSender = require('../data-sender.js')
 
-var url = "./test.jpeg";
+var url = "./files/game.jpg";
 console.log("Get image: " + url);
 
 // Send screenshot to opencv.
-var data = fileHandler.handle("./test.jpeg");
+var data = fileHandler.handle(url);
 console.log("recognized to: " + JSON.stringify(data));
 
 // Send opencv data to neural network.
