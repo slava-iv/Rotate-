2f2f774f2901dee0b97fa0aaeaffe0f291dd0ae8
vyacheslav.lonshchakov <Exeil@inbox.ru>
2016-11-26 09:52:24 +0300

    Reworked test.js

--- specs/test.js
+++ specs/test.js
@@ -6,9 +6,12 @@ var url = "./files/game.jpg";
 console.log("Get image: " + url);
 
 // Send screenshot to opencv.
-var data = fileHandler.handle(url);
-console.log("recognized to: " + JSON.stringify(data));
+var getDataPromise = fileHandler.handle(url);
 
-// Send opencv data to neural network.
-var result = neuralNetwork.send(data);
-console.log("neural network returns: " + JSON.stringify(result));
+getDataPromise.then(function (data) {
+  console.log("recognized to: " + JSON.stringify(data));
+
+  // Send opencv data to neural network.
+  var result = neuralNetwork.send(data);
+  console.log("neural network returns: " + JSON.stringify(result));
+})
